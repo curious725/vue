6bc52a31d15a3277d830822266b18c239bf3e7d6
curious725 <curious725@gmail.com>
2018-04-12 21:19:25 +0300

    make from global to local

--- vue.dev1.0.25/08_dynamic_components/main.js
+++ vue.dev1.0.25/08_dynamic_components/main.js
@@ -1,19 +1,17 @@
-Vue.component('square', {
-    template: '#square-template'
-})
-
-Vue.component('triangle', {
-    template: '#triangle-template'
-})
-
-Vue.component('circle', {
-    template: '#circle-template'
-})
-
-Vue.component('egg', {
-    template: '#egg-template'
-})
-
 new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+      square: {
+        template: '#square-template'
+      },
+      triangle: {
+        template: '#triangle-template'
+      },
+      circle: {
+        template: '#circle-template'
+      },
+      egg: {
+        template: '#egg-template'
+      }
+    }
 })
