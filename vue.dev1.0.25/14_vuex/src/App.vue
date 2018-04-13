aceae223278bad94a7ebd08b1fe60809d3a33656
curious725 <curious725@gmail.com>
2018-04-13 15:36:32 +0300

    register components in app

--- vue.dev1.0.25/14_vuex/src/App.vue
+++ vue.dev1.0.25/14_vuex/src/App.vue
@@ -10,8 +10,12 @@
 </template>
 
 <script>
+import Search from './components/Search.vue'
+import Results from './components/Results.vue'
+
 export default {
   name: 'app',
+  components: { Search, Results }
 }
 </script>
 
