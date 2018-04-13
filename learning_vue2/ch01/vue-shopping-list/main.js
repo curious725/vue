3c4f0d4a5d13ea262780d864f18699def52879a4
curious725 <curious725@gmail.com>
2018-04-13 23:20:07 +0300

    add new property newItem and method addItem

--- learning_vue2/ch01/vue-shopping-list/main.js
+++ learning_vue2/ch01/vue-shopping-list/main.js
@@ -3,11 +3,26 @@ var data = {
     { text: 'Bananas', checked: true },
     { text: 'Apples', checked: false }
   ],
-  title: 'My Shopping List'
+  title: 'My Shopping List',
+  newItem: ''
 };
 
 new Vue({
   el: '#app',
-  data: data
+  data: data,
+  methods: {
+    addItem: function() {
+      var text;
+
+      text = this.newItem.trim();
+      if (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = '';
+      }
+    }
+  }
 });
 
