d374a7a4701fd0cb534cee1f25fb2373506e5871
curious725 <curious725@gmail.com>
2018-04-13 15:33:47 +0300

    add new components

---
+++ vue.dev1.0.25/14_vuex/src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search">
+    <input type="text" class="search__input" v-model="query">
+    <button class="search__button">Find</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: ''
+    }
+  }
+}
+</script>
