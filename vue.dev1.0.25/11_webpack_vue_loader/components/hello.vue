60118923c4bca215bc761b11417c83a3e0e948d3
curious725 <curious725@gmail.com>
2018-04-13 12:20:38 +0300

    pass arguments to sass style

--- vue.dev1.0.25/11_webpack_vue_loader/components/hello.vue
+++ vue.dev1.0.25/11_webpack_vue_loader/components/hello.vue
@@ -12,8 +12,19 @@
   }
 </script>
 
-<style lang="sass">
-  h1 {
-    color: green;
-  }
+<style lang="sass?indentedSyntax=true">
+  h1
+    display: block
+    position: absolute
+    width: 500px
+    height: 100px
+    top: 50%
+    left: 50%
+    margin: -50px 0 0 -250px
+    font-size: 54px
+    line-height: 64px
+    font-family: 'Open Sans', sans-serif
+    font-weight: 300
+    color: #525255
+    text-align: center
 </style>
