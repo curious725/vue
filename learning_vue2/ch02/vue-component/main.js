b095900257ebf33e94ec75bf039efa46d252efb9
curious725 <curious725@gmail.com>
2018-04-15 15:13:21 +0300

    create components in main.js file

---
+++ learning_vue2/ch02/vue-component/main.js
@@ -0,0 +1,61 @@
+var data = {
+  items: [
+    { text: 'Bananas', checked: true },
+    { text: 'Apples', checked: false }
+  ],
+  title: 'My Shopping List',
+  newItem: ''
+};
+
+// Declaring components
+var ItemsComponent = Vue.extend({
+  data: function() {
+    return data;
+  },
+ template: '<ul>' +
+  '           <li v-for="item in items" :class="{ \'removed\': item.checked }">' +
+  '             <div class="checkbox">' +
+  '               <label>' +
+  '                 <input type="checkbox" v-model="item.checked"> {{ item.text }}' +
+  '               </label>' +
+  '             </div>' +
+  '           </li>' +
+  '         </ul>'
+});
+var ChangeTitleComponent = Vue.extend({
+  data: function() {
+    return data;
+  },
+  template: '      <em>Change  the title of  your  shopping  list  here</em>' +
+            '      <input v-model="title">'
+});
+var AddItemComponent = Vue.extend({
+  data: function() {
+    return data;
+  },
+  methods: {
+    addItem: function() {
+      var text;
+
+      text = this.newItem.trim();
+      if (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = '';    
+    },
+  }
+});
+
+// Registering components
+Vue.component('items-component', ItemsComponent);
+Vue.component('change-title-component', ChangeTitleComponent);
+Vue.component('add-item-component', AddItemComponent);
+
+// Instantiating a Vue instance
+new Vue({
+  el: '#app',
+  data: data
+});
+
